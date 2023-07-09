bcb9e455f5ae3f88b173c13534413e27fb033fa1
Zisan-uzum <uzummzisann@gmail.com>
2023-07-09 18:36:02 +0300

    add create workspace part

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
-<script>
-export default {}
+<script setup>
+
+import {ref} from 'vue'
+
+const newWorkspaceName = ref("")
+const workspaceList = ref([
+    { id: 1, name: "Test1" },
+    {id:2, name: "Test2"}
+])
+
+const createNewWorkspace = () => {
+    const randomId = Math.floor(Math.random() * 100)
+
+    workspaceList.value.push({
+        id: randomId,
+        name: newWorkspaceName.value
+    })
+}
+
 </script>
 
 
@@ -9,7 +26,11 @@ export default {}
 <h2>Recently viewed</h2>
 <h2>Workspaces</h2>
 
-
+<input type="text" v-model="newWorkspaceName"/>
+<button @click="createNewWorkspace">Create a Workspace</button>
+<ul>
+    <li v-for="workspace in workspaceList" :key="`workspace+ ${workspace.id}`"> {{ workspace.id }}:  {{ workspace.name }}</li>
+</ul>
 </template>
 
 <style>
